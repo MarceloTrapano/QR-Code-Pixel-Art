0abc2b14c035c947bf12819a9b93e4d62430312b
MarceloTrapano <kacper7866@gmail.com>
2026-07-27 23:57:56 +0200

    Create qr code creator

---
+++ src/qr_code/qr_code.py
@@ -0,0 +1,32 @@
+from interfaces import IQRCode
+import qrcode
+
+
+class QRCode(IQRCode):
+    def __init__(self, url: str, version: int = 6):
+        self.url = url
+        self.size = 3
+        self.version = version
+
+    def generate(self, filename: str = ""):
+        self.qr = qrcode.QRCode(
+            version=self.version,
+            box_size=self.size,
+            border=1,
+        )
+
+        self.qr.add_data(self.url)
+        self.qr.make(fit=True)
+
+        self.image = self.qr.make_image()
+        if self.version != self.qr._version:
+            self.version = self.qr._version
+
+        if filename:
+            self.image.save(filename)
+
+
+if __name__ == "__main__":
+    for ver in range(1, 25):
+        qr = QRCode("https://www.youtube.com", version=ver)
+        qr.generate(filename=f"dupa{ver}.png")
